35b13769d7be8e52fde345a800053bf95f3293a3
subzerox98415 <48654337+subzerox98415@users.noreply.github.com>
2020-12-08 06:43:52 +0100

    Újra feltöltött Repository

---
+++ A5IGRX_09_10/NyV_form/feldolgozo.php
@@ -0,0 +1,27 @@
+<?php
+if (isset($_GET['kuld'])) {
+	
+	echo "Name: ".$_GET['name']."<br>";
+	echo "Pin code: ".$_GET['pin']."<br>";
+	echo "Fruit: ".$_GET['gyum']."<br>";
+	echo "Age: ".$_GET['age']."<br>";
+	echo "Foot size: ".$_GET['lab']."<br>";
+	echo "Confidence: ".$_GET['onb']."<br>";
+	
+	}
+?>
+
+<html>
+
+<head>
+	<title>Feldolgozo</title>
+	
+	
+</head>
+
+<body>
+	<br>
+	<a href="urlap.html"><button type="button">Vissza</button></a>
+</body>
+
+</html>
